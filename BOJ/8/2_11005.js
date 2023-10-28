d21e0493208eeba6a2e0045f4d22e65685c1c650
rmawogns <evolui@naver.com>
2023-10-28 23:52:18 +0900

    BOJ 8단계 2

---
+++ BOJ/8/2_11005.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let [num, divider] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((item) => Number(item));
+
+let ans = num.toString(divider).toUpperCase();
+
+console.log(ans);
