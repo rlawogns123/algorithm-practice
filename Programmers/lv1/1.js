673329e5e1d7f0f5a44e3c37e2cd8f940bd5407a
unknown <rlawogns123@naver.com>
2024-04-20 16:56:56 +0900

    lv1 1

---
+++ Programmers/lv1/1.js
@@ -0,0 +1,16 @@
+// 폰켓몬
+
+function solution(nums) {
+  var answer = [];
+  var max = nums.length / 2;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (answer.length < max) {
+      if (!answer.includes(nums[i])) {
+        answer.push(nums[i]);
+      }
+    }
+  }
+
+  return answer.length;
+}
