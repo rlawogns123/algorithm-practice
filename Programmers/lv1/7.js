6e6e8b07484ffee8b0d30a4d2bdec0953372a207
rlawogns123 <evolui@naver.com>
2024-04-25 23:19:22 +0900

    programmers lv1 1

---
+++ Programmers/lv1/7.js
@@ -0,0 +1,13 @@
+// 문자열 내 마음대로 정렬하기
+
+function solution(strings, n) {
+  const answer = strings.sort((a, b) => {
+    if (a[n] > b[n]) return 1;
+    if (a[n] < b[n]) return -1;
+    if (a[n] === b[n]) {
+      if (a > b) return 1;
+      else return -1;
+    }
+  });
+  return answer;
+}
