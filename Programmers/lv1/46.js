26e1bafd24c81a884a1b5646b068214601b857d1
rlawogns123 <evolui@naver.com>
2024-06-04 23:52:56 +0900

    progreammers lv1 1

--- Programmers/lv1/46.js
+++ Programmers/lv1/46.js
@@ -2,55 +2,12 @@
 
 function solution(lottos, win_nums) {
   var answer = [];
-  let data = 0;
-  for (let i = 0; i < lottos.length; i++) {
-    for (let j = 0; j < win_nums.length; j++) {
-      if (lottos[i] === win_nums[j]) {
-        data++;
-        win_nums.splice(j, 1);
-      }
-    }
-  }
-  const best = lottos.filter((e) => e === 0).length + data;
-  switch (best) {
-    case 6:
-      answer.push(1);
-      break;
-    case 5:
-      answer.push(2);
-      break;
-    case 4:
-      answer.push(3);
-      break;
-    case 3:
-      answer.push(4);
-      break;
-    case 2:
-      answer.push(5);
-      break;
-    default:
-      answer.push(6);
-      break;
-  }
-  switch (data) {
-    case 6:
-      answer.push(1);
-      break;
-    case 5:
-      answer.push(2);
-      break;
-    case 4:
-      answer.push(3);
-      break;
-    case 3:
-      answer.push(4);
-      break;
-    case 2:
-      answer.push(5);
-      break;
-    default:
-      answer.push(6);
-      break;
-  }
+  const wins = lottos.filter((e) => win_nums.includes(e)).length;
+  const data = lottos.filter((e) => e === 0).length + wins;
+
+  const worst = 7 - wins >= 6 ? 6 : 7 - wins;
+  const best = 7 - data >= 6 ? 6 : 7 - data;
+  answer = [best, worst];
+
   return answer;
 }
