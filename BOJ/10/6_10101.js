95e34a132cf930cfc1023c7b5af2bc85f7e1ab1e
rlawogns123 <evolui@naver.com>
2024-07-18 22:57:03 +0900

    BOJ 10101.js

---
+++ BOJ/10/6_10101.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const [a, b, c] = fs
+  .readFileSync("/dev/stdin", "utf-8")
+  .trim()
+  .split("\n")
+  .map(Number);
+
+if (a + b + c !== 180) {
+  console.log("Error");
+} else {
+  if (a === b) {
+    if (a === c) {
+      console.log("Equilateral");
+    } else {
+      console.log("Isosceles");
+    }
+  } else {
+    if (a === c || b === c) {
+      console.log("Isosceles");
+    } else {
+      console.log("Scalene");
+    }
+  }
+}
