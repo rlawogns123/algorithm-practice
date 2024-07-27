4f9fbf9c009006cfe850e93d088dfa0897f9f81d
rlawogns123 <evolui@naver.com>
2024-07-27 23:49:13 +0900

    BOJ 24267.js

---
+++ BOJ/11/6_24267.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let num = BigInt(input);
+
+console.log(`${num * num * num}`);
+console.log("3");
